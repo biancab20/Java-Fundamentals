c4fbeaf5f8c38c0a04915ede1d4be1ece510e481
biancab20 <705366@student.inholland.nl>
2023-10-26 15:11:12 +0200

    lesson1 exercise2 incomplete

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public String name;
+    public boolean presence;
+
+    public Student(String name, boolean presence) {
+        this.name = name;
+        this.presence = presence;
+    }
+}
